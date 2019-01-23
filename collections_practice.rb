22835211cce2e72ee839e80f12b1fad6aa009a65
Derek Cerretani <derekcerretani@gmail.com>
2019-01-23 01:07:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,7 +40,8 @@ end
 
 def add_s(array)
   second_element = array.slice(1, 1)
-  array.each {|word| word << "s"}
-  array[1] = second_element
+  array.each do |word| word << "s"
+  end
+  array[1] = second_element.join(" ")
   array
 end
